8f6be0b9be81b35456af116fc84aa98991100b74
yuanjie.xia@hotmail.com <yuanjie.xia@hotmail.com@db5b3a8f-9962-ec3d-6b9c-3cadb9b9d16c>
2011-12-27 05:24:11 +0000

    创建时暂停

--- Proc/Process.c
+++ Proc/Process.c
@@ -38,6 +38,23 @@ BOOLEAN EPROCESS__PPEB(PEPROCESS pEproc, PPEB* pPeb)
 	*pPeb = (PPEB)*( (ULONG*)((char*)pEproc + nPos) );
 	return TRUE;
 }
+BOOLEAN EPROCESS__ThreadListHead(PEPROCESS pEproc, PLIST_ENTRY* pThreadList)
+{
+	ULONG			nPos	= 0;
+
+	if(NULL == pEproc)
+		return FALSE;
+	switch(GetBuildNumber())
+	{
+	case 2600: // Windows XP Kernel Version 2600 (Service Pack 2) UP Free x86 compatible
+		nPos = 0x190;
+		break;
+	default:
+		return FALSE;
+	}
+	*pThreadList = (PLIST_ENTRY)((char*)pEproc + nPos);
+	return TRUE;
+}
 // 获取Imagebase地址
 BOOLEAN PEB__ImageBaseAddress(PPEB pPeb, PVOID* pBase)
 {
@@ -346,4 +363,82 @@ PVOID	GetModuleBaseAddress(PEPROCESS pEproc, PUNICODE_STRING pDllName)
 		return pBaseName;
 	}
 	return NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// 线程操作
+BOOLEAN CONTAINING_RECORD__ETHREAD(PVOID pEntery, PETHREAD* pEThread)
+{
+	LONG		nPos		= 0;
+
+	if(NULL == pEThread)
+		return FALSE;
+
+	switch(GetBuildNumber())
+	{
+	case 2600:	// Windows XP Kernel Version 2600 (Service Pack 2) UP Free x86 compatible
+		nPos = -0x22c;
+		break;
+	default:
+		return FALSE;
+	}
+	*pEThread = (PETHREAD)((char*)pEntery + nPos);
+	return TRUE;
+}
+
+BOOLEAN ETHREAD__Tcb(PETHREAD EThread, PKTHREAD* pTcb)
+{
+	LONG		nPos		= 0;
+
+	if(NULL == EThread)
+		return FALSE;
+
+	switch(GetBuildNumber())
+	{
+	case 2600:	// Windows XP Kernel Version 2600 (Service Pack 2) UP Free x86 compatible
+		nPos = 0x0;
+		break;
+	default:
+		return FALSE;
+	}
+	*pTcb = (PKTHREAD)((char*)EThread + nPos);
+	return TRUE;
+}
+
+BOOLEAN KTHREAD__SuspendCount(PKTHREAD pTcb, PCHAR* pSuspendCount)
+{
+	LONG		nPos		= 0;
+
+	if(NULL == pTcb)
+		return FALSE;
+
+	switch(GetBuildNumber())
+	{
+	case 2600:	// Windows XP Kernel Version 2600 (Service Pack 2) UP Free x86 compatible
+		nPos = 0x1b9;
+		break;
+	default:
+		return FALSE;
+	}
+	*pSuspendCount = (PCHAR)((char*)pTcb + nPos);
+	return TRUE;
+}
+
+BOOLEAN KTHREAD__SuspendApc(PKTHREAD pTcb, PKAPC* pSuspendApc)
+{
+	LONG		nPos		= 0;
+
+	if(NULL == pTcb)
+		return FALSE;
+
+	switch(GetBuildNumber())
+	{
+	case 2600:	// Windows XP Kernel Version 2600 (Service Pack 2) UP Free x86 compatible
+		nPos = 0x16c;
+		break;
+	default:
+		return FALSE;
+	}
+	*pSuspendApc = (PKAPC)((char*)pTcb + nPos);
+	return TRUE;
 }
